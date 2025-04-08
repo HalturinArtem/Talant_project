d57b19373c520785189413c1dd8631e68b4d6dd5
HalturinArtem <ah704212@gmail.com>
2025-04-08 13:43:03 +0300

    Third commit

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 # Тестовый файл
+
+import os
+import time
+import platform
+import ctypes
+import cv2 # Библиотека OpenCV для работы с веб-камерой
+from pynput import mouse, keyboard # Библиотека для отслеживания клавиатуры и мыши
+
+# Время бездействия в секундах перед выключением монитора
+IDLE_THRESHOLD = 30
+FACE_DETECTION_THRESHOLD = 5 # Время (в секундах), после которого выключится монитор, если нет лица
+OS_TYPE = platform.system()
+last_active_time = time.time() # Время последней активности
+last_face_detected_time = time.time()  # Время последнего обнаруженного лица
