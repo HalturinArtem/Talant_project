adeaf01da6f5a9a4d8b17a777a85bb218b27807b
HalturinArtem <ah704212@gmail.com>
2025-04-08 13:43:03 +0300

    Forth commit

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 # Тестовый файл
+
+import os
+import time
+import platform
+import ctypes
+import cv2 # Библиотека OpenCV для работы с веб-камерой
+from pynput import mouse, keyboard # Библиотека для отслеживания клавиатуры и мыши
+
